64ca371839ff275cf0b618e91ea4e8435c25e43f
Vicky Steeves <vicky.steeves@nyu.edu>
2016-08-17 13:05:25 -0400

    Move to vickysteeves.com

--- conf.py
+++ conf.py
@@ -21,7 +21,7 @@ BLOG_AUTHOR = "Vicky Steeves"  # (translatable)
 BLOG_TITLE = "VICKY STEEVES"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://vickysteeves.github.io/Nikola-Site/"
+SITE_URL = "http://vickysteeves.com"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://example.com/"
