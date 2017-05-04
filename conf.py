098efb3b30d27a928a2eb98b66be29f7c5199f66
Vicky Steeves <victoriaisteeves@gmail.com>
2017-05-04 18:37:23 +0000

    changed 'output folder' to 'public'

--- conf.py
+++ conf.py
@@ -579,7 +579,7 @@ GITHUB_COMMIT_SOURCE = True
 # Where the output site should be located
 # If you don't use an absolute path, it will be considered as relative
 # to the location of conf.py
-# OUTPUT_FOLDER = 'output'
+OUTPUT_FOLDER = 'public'
 
 # where the "cache" of partial generated content should be located
 # default: 'cache'
